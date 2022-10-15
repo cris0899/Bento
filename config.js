5969271e3907dbcf28d9bf73bd3bb638c97bea70
Cristhian Morales <96213145+cris0899@users.noreply.github.com>
2022-10-15 15:12:00 -0600

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
 	// General
 	name: 'Cristhian',
 	imageBackground: false,
-	openInNewTab: true,
+	openInNewTab: false,
 	twelveHourFormat: true,
 
 	// Greetings
@@ -77,9 +77,9 @@ const CONFIG = {
 		},
 		{
 			id: '5',
-			name: 'Reddit',
+			name: 'Archive',
 			icon: 'glasses',
-			link: 'https://reddit.com',
+			link: 'https://archive.org',
 		},
 		{
 			id: '6',
