38c27cefdb1c4fb854074b8db9d12d193b72d1a0
Cristhian Morales <96213145+cris0899@users.noreply.github.com>
2022-10-15 15:07:14 -0600

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ const CONFIG = {
 	greetingNight: 'Go to Sleep!',
 
 	// Layout
-	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
+	bentoLayout: 'buttons', // 'bento', 'lists', 'buttons'
 
 	// Weather
 	weatherKey: '4e0e2c8e94c67294e779b959d3ffe3c3', // Write here your API Key
